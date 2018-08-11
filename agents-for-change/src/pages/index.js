b30f0b8372b533984aac777e940aba7ed34d2115
andrwsalcdo <asalcedo07@gmail.com>
2018-08-11 11:52:38 -0400

    add gatsby bootstrap starter boilerplate

--- agents-for-change/src/pages/index.js
+++ agents-for-change/src/pages/index.js
@@ -1,13 +1,67 @@
+import { graphql } from 'gatsby'
 import React from 'react'
-import Link from 'gatsby-link'
+import get from 'lodash/get'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+import Post from 'templates/Post'
+import Meta from 'components/Meta'
+import Layout from 'components/Layout'
 
-export default IndexPage
+const BlogIndex = ({ data, location }) => {
+  const posts = get(data, 'remark.posts')
+  return (
+    <Layout location={location}>
+      <Meta site={get(data, 'site.meta')} />
+      {posts.map(({ post }, i) => (
+        <Post
+          data={post}
+          options={{
+            isIndex: true,
+          }}
+          key={i}
+        />
+      ))}
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    site {
+      meta: siteMetadata {
+        title
+        description
+        url: siteUrl
+        author
+        twitter
+        adsense
+      }
+    }
+    remark: allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      posts: edges {
+        post: node {
+          html
+          frontmatter {
+            layout
+            title
+            path
+            category
+            tags
+            description
+            date(formatString: "YYYY/MM/DD")
+            image {
+              childImageSharp {
+                fixed(width: 500) {
+                  ...GatsbyImageSharpFixed_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
